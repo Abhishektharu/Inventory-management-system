752279534fbf4999a4e51bea505b71ce3f63a467
unknown <abhishekchaudhary229@gmail.com>
2023-07-08 20:48:34 +0545

    working product add image

--- partials/app_sidebar.php
+++ partials/app_sidebar.php
@@ -9,12 +9,6 @@
 
     <!--menu item-->
     <ul>
-        <li>
-            <a href="#" class="btn active" >
-                <span class="icon"><i class="fas fa-home"></i></span>
-                <span class="item">Home</span>
-            </a>
-        </li>
         
         <li>
             <a href="dashboard.php" class="btn">
